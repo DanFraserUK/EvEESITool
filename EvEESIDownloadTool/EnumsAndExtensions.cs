4189806f1117b8d49b9d5dd03e2e1a1efe100194
DanFraserUK <danielfras@gmail.com>
2019-01-13 00:30:20 +0000

    Started adding back in SDE data, including a search function

---
+++ EvEESIDownloadTool/EnumsAndExtensions.cs
@@ -0,0 +1,50 @@
+﻿using ESI.NET;
+using ESI.NET.Enumerations;
+using ESI.NET.Models.Assets;
+using ESI.NET.Models.Character;
+using ESI.NET.Models.Corporation;
+using ESI.NET.Models.Industry;
+using ESI.NET.Models.Location;
+using ESI.NET.Models.Market;
+using ESI.NET.Models.SSO;
+using ESI.NET.Models.Wallet;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using System.Linq;
+using ESI.NET.Models.Skills;
+using ESI.NET.Models.Clones;
+using ESI.NET.Models.Fittings;
+using ESI.NET.Models;
+using System.Reflection;
+using System.Timers;
+
+namespace EvEESITool
+{
+	public class Enums
+	{
+
+	}
+	public static class Extensions
+	{
+		public static InvTypes Search(this List<InvTypes> item, int searchValue)
+		{
+			// list.Find(i => i.Property == value);
+			return item.Find(i => i.TypeID == searchValue);
+		}
+		public static InvItems Search(this List<InvItems> item, int searchValue)
+		{
+			// list.Find(i => i.Property == value);
+			return item.Find(i => i.ItemID == searchValue);
+		}
+		public static StaStations Search(this List<StaStations> item, int searchValue)
+		{
+			// list.Find(i => i.Property == value);
+			return item.Find(i => i.StationID == searchValue);
+		}
+	}
+}
